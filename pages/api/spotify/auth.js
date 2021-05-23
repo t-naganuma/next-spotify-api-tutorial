454912b246f3eeb2384130f08bb022cebf9b9687
t-naganuma <takuya.naganuma@gmail.com>
2021-05-23 22:39:56 +0900

    スコープの追加

--- pages/api/spotify/auth.js
+++ pages/api/spotify/auth.js
@@ -2,9 +2,12 @@ import axios from 'axios'
 
 export default async (req, res) => {
   const params = new URLSearchParams();
+  const scopes = 'user-read-recently-played user-top-read playlist-modify-public streaming'
   params.append('client_id', process.env.CLIENT_ID);
   params.append('response_type', 'code');
   params.append('redirect_uri', 'http://localhost:3000');
+  params.append('scope', scopes);
+  params.append('state', 'state');
   const endpoint = 'https://accounts.spotify.com/authorize';
   const response = await axios.get(endpoint, {params});
 
