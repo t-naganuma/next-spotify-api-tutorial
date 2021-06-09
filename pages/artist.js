bdb8cca23e68329dce8e3b3c57e2d0866aac15ce
t-naganuma <takuya.naganuma@gmail.com>
2021-06-09 19:39:18 +0900

    メソッド名変更

--- pages/artist.js
+++ pages/artist.js
@@ -38,52 +38,13 @@ export default function artist() {
     getArtist();
   }, []);
 
-  const getArtistWithTerm = (term) => {
+  const getArtistByTerm = (term) => {
     const accessToken = localStorage.getItem('accessToken');
     const endpoint = `https://api.spotify.com/v1/me/top/artists?time_range=${term}`;
     const headers = { Authorization: `Bearer ${accessToken}` };
     axios
       .get(endpoint, { headers })
       .then((res) => {
-        console.log(res);
-        res.data.items.push({
-          external_urls: {
-            spotify: 'https://open.spotify.com/artist/0Ak6DLKHtpR6TEEnmcorKA',
-          },
-          followers: {
-            href: null,
-            total: 659532,
-          },
-          genres: [
-            'indie rock',
-            'modern alternative rock',
-            'modern rock',
-            'rock',
-          ],
-          href: 'https://api.spotify.com/v1/artists/0Ak6DLKHtpR6TEEnmcorKA',
-          id: '0Ak6DLKHtpR6TEEnmcorKA',
-          images: [
-            {
-              height: 640,
-              url: 'https://i.scdn.co/image/ab6761610000e5ebd59ae500cc9fcdc5911059ae',
-              width: 640,
-            },
-            {
-              height: 320,
-              url: 'https://i.scdn.co/image/ab67616100005174d59ae500cc9fcdc5911059ae',
-              width: 320,
-            },
-            {
-              height: 160,
-              url: 'https://i.scdn.co/image/ab6761610000f178d59ae500cc9fcdc5911059ae',
-              width: 160,
-            },
-          ],
-          name: 'testtest',
-          popularity: 64,
-          type: 'artist',
-          uri: 'spotify:artist:0Ak6DLKHtpR6TEEnmcorKA',
-        });
         setArtists(res.data.items);
       })
       .catch((error) => {
@@ -95,7 +56,6 @@ export default function artist() {
   };
 
   const displayArtists = artists.map((artist, i) => {
-    console.log(artist);
     return (
       <li key={artist.id} className={contentStyles.list}>
         <span className={contentStyles.order_number}>{i + 1}</span>
@@ -116,26 +76,26 @@ export default function artist() {
     <div className={styles.container}>
       <main>
         <section className={contentStyles.sec_top}>
-          <Image
+          {/* <Image
             src="/artist.jpg"
             alt="Top Artist Page"
             width={1000}
             height={600}
-          />
+          /> */}
           <h1 className={contentStyles.heading1}>Top Artists</h1>
           <button
             className={buttonStyles.button}
-            onClick={() => getArtistWithTerm('short_term')}>
+            onClick={() => getArtistByTerm('short_term')}>
             short term
           </button>
           <button
             className={buttonStyles.button}
-            onClick={() => getArtistWithTerm('medium_term')}>
+            onClick={() => getArtistByTerm('medium_term')}>
             medium term
           </button>
           <button
             className={buttonStyles.button}
-            onClick={() => getArtistWithTerm('long_term')}>
+            onClick={() => getArtistByTerm('long_term')}>
             long term
           </button>
           <ul>{displayArtists}</ul>
