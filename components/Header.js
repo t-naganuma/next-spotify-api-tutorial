4739d98d475b3cdde19c23364274afef662ecf83
t-naganuma <takuya.naganuma@gmail.com>
2021-06-26 14:53:40 +0900

    feat:Tracks表示

--- components/Header.js
+++ components/Header.js
@@ -34,7 +34,7 @@ const Header = (props) => {
     <header>
       <nav className={headerStyles.nav}>
         <div className={headerStyles.nav_container}>
-          <h1 className={headerStyles.heading1}>Top Artists</h1>
+          <h1 className={headerStyles.heading1}>{props.title}</h1>
           <div>
             <NavList currentPage={props.currentPage} />
           </div>
--- pages/tracks.js
+++ pages/tracks.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
 import axios from 'axios';
 import checkExpiration from '../lib/checkExpiration';
 import config from '../config';
@@ -15,14 +14,17 @@ const Modal = (props) => {
 
   const handleCloseModal = () => {
     props.closeModal();
-  }
+  };
 
   return (
     <div className={`${modalStyles.modal} ${modalStyles.is_show}`}>
       <div className={modalStyles.body}>
         <p className={modalStyles.text}>プレイリストを作成しました！</p>
         <div className={modalStyles.button_area}>
-          <button type="button" onClick={handleCloseModal} className={modalStyles.close}>
+          <button
+            type="button"
+            onClick={handleCloseModal}
+            className={modalStyles.close}>
             閉じる
           </button>
         </div>
@@ -31,11 +33,11 @@ const Modal = (props) => {
   );
 };
 
-export default function artist() {
-  const [artists, setArtists] = useState([]);
+export default function tracks() {
+  const [tracks, setTracks] = useState([]);
   const [flag, setFlag] = useState(false);
   useEffect(() => {
-    const getArtist = () => {
+    const getTracks = () => {
       try {
         const accessToken = localStorage.getItem('accessToken');
         if (!accessToken) {
@@ -44,136 +46,140 @@ export default function artist() {
         checkExpiration();
 
         // Spotify ユーザーのTOP Artist取得
-        const endpoint = `${config.API_URL}/me/top/artists`;
+        const endpoint = `${config.API_URL}/me/top/tracks`;
         const headers = { Authorization: `Bearer ${accessToken}` };
-        axios.get(endpoint, { headers })
+        axios
+          .get(endpoint, { headers })
           .then((res) => {
-            setArtists(res.data.items);
+            setTracks(res.data.items);
           })
           .catch((error) => {
             throw error.response.status;
           });
-          
-      } catch(error) {
+      } catch (error) {
         if (error === 'アクセストークンを取得できていません') {
           alert(`サインインしていません。\nサインインしてください。`);
           location.href = '/';
         }
       }
-    }
-    getArtist();
+    };
+    getTracks();
   }, []);
 
-  const getArtistByTerm = (term) => {
-    const accessToken = localStorage.getItem('accessToken');
-    const endpoint = `${config.API_URL}/me/top/artists?time_range=${term}`;
-    const headers = { Authorization: `Bearer ${accessToken}` };
-    try {
-      axios.get(endpoint, { headers })
-        .then((res) => {
-          setArtists(res.data.items);
-        })
-        .catch((error) => {
-          throw error.response;
-        });
-    } catch(error) {
-      const errorObject = JSON.stringify(error.data.error);
-      const statusCode = error.data.error.status;
+  // const getArtistByTerm = (term) => {
+  //   const accessToken = localStorage.getItem('accessToken');
+  //   const endpoint = `${config.API_URL}/me/top/artists?time_range=${term}`;
+  //   const headers = { Authorization: `Bearer ${accessToken}` };
+  //   try {
+  //     axios
+  //       .get(endpoint, { headers })
+  //       .then((res) => {
+  //         setArtists(res.data.items);
+  //       })
+  //       .catch((error) => {
+  //         throw error.response;
+  //       });
+  //   } catch (error) {
+  //     const errorObject = JSON.stringify(error.data.error);
+  //     const statusCode = error.data.error.status;
 
-      let m = alertsByErrorCode(statusCode);
-      alert(`${errorObject}\n\n${m}`);
-      location.href = '/';
-    }
-  };
+  //     let m = alertsByErrorCode(statusCode);
+  //     alert(`${errorObject}\n\n${m}`);
+  //     location.href = '/';
+  //   }
+  // };
 
-  const displayArtists = artists.map((artist, i) => {
+  const displayTracks = tracks.map((track, i) => {
+    console.log(track)
     return (
-      <li key={artist.id} className={contentStyles.list}>
+      <li key={track.id} className={contentStyles.list}>
         <span className={contentStyles.order_number}>{i + 1}</span>
         <img
           className={contentStyles.img}
-          src={artist.images[1].url}
-          alt={artist.name}
+          src={track.album.images[1].url}
+          alt={track.name}
         />
         <span className={contentStyles.artist_info}>
-          <p className={contentStyles.content_name}>{artist.name}</p>
-          <p className={contentStyles.genre_info}>{artist.genres}</p>
+          <p className={contentStyles.content_name}>{track.name}</p>
+          <p className={contentStyles.genre_info}>{track.artists[0].name}</p>
         </span>
       </li>
     );
   });
 
-  const createPlaylist = async () => {
-    const accessToken = localStorage.getItem('accessToken');
-    const headers = { Authorization: `Bearer ${accessToken}` };
+  // const createPlaylist = async () => {
+  //   const accessToken = localStorage.getItem('accessToken');
+  //   const headers = { Authorization: `Bearer ${accessToken}` };
 
-    // TODO axiosで通信する部分をモジュール化する, libにまとめる。
-    try {
-      // user_idを取得
-      const endpoint = `${config.API_URL}/me`;
-      const user_id = await axios.get(endpoint, { headers })
-        .then((res) => {
-          return res.data.id;
-        })
-        .catch((error) => {
-          throw error.response;
-        });
+  //   // TODO axiosで通信する部分をモジュール化する, libにまとめる。
+  //   try {
+  //     // user_idを取得
+  //     const endpoint = `${config.API_URL}/me`;
+  //     const user_id = await axios
+  //       .get(endpoint, { headers })
+  //       .then((res) => {
+  //         return res.data.id;
+  //       })
+  //       .catch((error) => {
+  //         throw error.response;
+  //       });
 
-      // プレイリスト名、説明
-      const playlistsConfig = {
-        name: 'Playlists of your favorite artists',
-        description: 'Playlists of your favorite artists',
-        public: true,
-      };
+  //     // プレイリスト名、説明
+  //     const playlistsConfig = {
+  //       name: 'Playlists of your favorite artists',
+  //       description: 'Playlists of your favorite artists',
+  //       public: true,
+  //     };
 
-      // 空のplaylistを作成,idを取得
-      const playlistId = await axios
-        .post(`${config.API_URL}/users/${user_id}/playlists`, playlistsConfig, {
-          headers,
-        })
-        .then((res) => {
-          return res.data.id;
-        })
-        .catch((error) => {
-          throw error.response;
-        });
+  //     // 空のplaylistを作成,idを取得
+  //     const playlistId = await axios
+  //       .post(`${config.API_URL}/users/${user_id}/playlists`, playlistsConfig, {
+  //         headers,
+  //       })
+  //       .then((res) => {
+  //         return res.data.id;
+  //       })
+  //       .catch((error) => {
+  //         throw error.response;
+  //       });
 
-      const uris = await Promise.all(
-        artists.map(async (artist) => {
-          const topTrackEndpoint = `${config.API_URL}/artists/${artist.id}/top-tracks?market=JP`;
-          return await axios
-            .get(topTrackEndpoint, { headers })
-            .then((res) => res.data.tracks[0].uri)
-            .catch((error) => {
-              throw error.response;
-            });
-        })
-        
-      );
+  //     const uris = await Promise.all(
+  //       artists.map(async (artist) => {
+  //         const topTrackEndpoint = `${config.API_URL}/artists/${artist.id}/top-tracks?market=JP`;
+  //         return await axios
+  //           .get(topTrackEndpoint, { headers })
+  //           .then((res) => res.data.tracks[0].uri)
+  //           .catch((error) => {
+  //             throw error.response;
+  //           });
+  //       })
+  //     );
 
-      // 曲のtrack uriを入れる
-      const tracks2 = { uris };
-      const responseStatus = await axios
-        .post(`${config.API_URL}/playlists/${playlistId}/tracks`, tracks2, { headers })
-        .then((res) => {
-          return res.status;
-        })
-        .catch((error) => {
-          throw error.response;
-        });
+  //     // 曲のtrack uriを入れる
+  //     const tracks2 = { uris };
+  //     const responseStatus = await axios
+  //       .post(`${config.API_URL}/playlists/${playlistId}/tracks`, tracks2, {
+  //         headers,
+  //       })
+  //       .then((res) => {
+  //         return res.status;
+  //       })
+  //       .catch((error) => {
+  //         throw error.response;
+  //       });
 
-      if (responseStatus === 201) {
-        setFlag(true);
-      }
-    } catch(error) {
-      const errorObject = JSON.stringify(error.data.error);
-      const statusCode = error.data.error.status;
+  //     if (responseStatus === 201) {
+  //       setFlag(true);
+  //     }
+  //   } catch (error) {
+  //     const errorObject = JSON.stringify(error.data.error);
+  //     const statusCode = error.data.error.status;
 
-      let m = alertsByErrorCode(statusCode);
-      alert(`${errorObject}\n\n${m}`);
-      location.href = '/';
-    }
-  };
+  //     let m = alertsByErrorCode(statusCode);
+  //     alert(`${errorObject}\n\n${m}`);
+  //     location.href = '/';
+  //   }
+  // };
 
   function alertsByErrorCode(status) {
     const messagesByErrorCode = {
@@ -191,11 +197,11 @@ export default function artist() {
 
   const closeModal = () => {
     setFlag(false);
-  }
+  };
 
   return (
     <div className={styles.container}>
-      <Header currentPage={'artist'} title={'Top Artists'} />
+      <Header currentPage={'tracks'} title={'Top Tracks'} />
       <main className={styles.main}>
         <section className={artistStyles.sec_artist}>
           <div className={contentStyles.time_range_selector}>
@@ -215,14 +221,14 @@ export default function artist() {
               All time
             </button>
           </div>
-          <ul>{displayArtists}</ul>
+          <ul>{displayTracks}</ul>
           <div className={contentStyles.create_playlists}>
             <div className={contentStyles.create_playlists_inner}>
-              <button
+              {/* <button
                 className={`${buttonStyles.button} ${buttonStyles.dark}`}
                 onClick={createPlaylist}>
                 Create Playlist
-              </button>
+              </button> */}
             </div>
           </div>
         </section>
